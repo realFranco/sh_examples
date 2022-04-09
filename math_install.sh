dffd96967998e77955cbade7d0d51dfeb6faf9ef
Franco Gil <45880759+realFranco@users.noreply.github.com>
2022-04-09 11:50:30 -0400

    Add math environment
    Add latex & R studio dependencies

---
+++ math_install.sh
@@ -0,0 +1,22 @@
+# Install for a FaCyT's math girl
+# April 9th, 2022
+
+# Install .deb a file & dependencies:
+#
+# sudo dpkg -i /path/to/deb/file.deb
+# sudo apt-get install -f
+
+# Distro focal-fosa 20.04
+
+# Install LaTex
+# REF: https://linuxconfig.org/how-to-install-latex-on-ubuntu-20-04-focal-fossa-linux
+sudo apt install texlive-latex-extra
+
+# Install R
+# REF: https://linuxize.com/post/how-to-install-r-on-ubuntu-20-04/
+
+
+# Install R Studio
+# REF: https://www.rstudio.com
+# REF: https://www.rstudio.com/products/rstudio/download/
+
